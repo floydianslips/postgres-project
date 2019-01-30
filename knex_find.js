b36dbe5a7318ae9941497af230ca300ff7480d5d
joshua <josh@joshuadennis.ca>
2019-01-30 01:45:02 +0000

    knex lookup working

---
+++ knex_find.js
@@ -0,0 +1,15 @@
+const config = require("./knexfile");
+const knex = require('knex')(config);
+const args = process.argv.slice(2)[0];
+
+knex
+.select('first_name', 'last_name', 'birthdate')
+.from('famous_people').
+where('first_name', '=', args).
+orWhere('last_name', '=', args)
+.asCallback(function(err, rows){
+  let rowData = rows[0];
+  console.log("The famous person is: " + rowData.first_name + rowData.last_name + ", whose birthday is: " + rowData.birthdate.toString().slice(0, 15))
+  knex.destroy();
+  // rows.forEach()
+})
